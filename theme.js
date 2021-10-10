5568a34705e683639f1934dc4316c407b537eea8
Shaunak Guchhait <sguchhait0806@gmail.com>
2021-10-10 15:46:19 +0530

    transitions added

--- theme.js
+++ theme.js
@@ -1,22 +1,15 @@
-// function to set a given theme/color-scheme
-function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
-    } else {
-        setTheme('theme-dark');
-    }
-}
+function enter(element, transition) {
+    element.classList.remove('hidden');
 
-// Immediately invoked function to set the theme on initial load
-(function() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-    } else {
-        setTheme('theme-light');
-    }
-})();
+    element.classList.add(`${transition}-enter`);
+    element.classList.add(`${transition}-enter-start`);
+
+    requestAnimationFrame(() => {
+        element.classList.remove(`${transition}-enter-start`);
+        element.classList.add(`${transition}-enter-end`);
+
+        // Wait until the transition is over...
+
+        element.classList.remove(`${transition}-enter-end`);
+        element.classList.remove(`${transition}-enter`);
+    });
