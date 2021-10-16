b4076571f2147658c78594eab964bda2efe9d177
Shaunak Guchhait <sguchhait0806@gmail.com>
2021-10-16 12:04:42 +0530

    project completed

--- Javascript/windowscroll.js
+++ Javascript/windowscroll.js
@@ -1,19 +1,3 @@
-//scroll up btn
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    scrollFunction()
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 const scrollToTop = () => {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
